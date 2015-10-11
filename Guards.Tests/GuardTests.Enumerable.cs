649750477b89f04b8909905e95c733499fd5bc80
thomasgalliker <thomas@bluewin.ch>
2015-10-11 21:38:18 +0200

    ArgumentNotNullOrEmpty for IEnumerable

---
+++ Guards.Tests/GuardTests.Enumerable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Guards.Tests
+{
+    public partial class GuardTests
+    {
+        [Fact]
+        public void ArgumentNotNullOrEmptyThrowsIfEnumerableIsEmpty()
+        {
+            // Arrange
+            string argumentName = "list";
+            var list = new List<string>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentException>(() => Guard.ArgumentNotNullOrEmpty(list, argumentName));
+
+            // Assert
+            ex.ParamName.Should().BeEquivalentTo(argumentName);
+            ex.Message.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmptyThrowsIfEnumerableIsEmptyWithExpression()
+        {
+            // Arrange
+            string argumentName = "list";
+            var list = new List<string>();
+
+            // Act
+            var ex = Assert.Throws<ArgumentException>(() => Guard.ArgumentNotNullOrEmpty(() => list));
+
+            // Assert
+            ex.ParamName.Should().BeEquivalentTo(argumentName);
+            ex.Message.Should().NotBeNullOrEmpty();
+        }
+    }
+}
