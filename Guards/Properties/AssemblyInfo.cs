7436dee1d4abcff76af07d83244921e41d05925e
thomasgalliker <thomas@bluewin.ch>
2015-10-26 20:14:55 +0100

    Copyright correction

--- Guards/Properties/AssemblyInfo.cs
+++ Guards/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Guards")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
