cff0db2c646d2fbed0293dcadc4172d33d01acff
thomasgalliker <thomas@bluewin.ch>
2016-10-10 21:42:42 +0200

    Update AssemblyInfo

--- Guards/Properties/AssemblyInfo.cs
+++ Guards/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -25,6 +23,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.13")]
+[assembly: AssemblyFileVersion("1.0.13")]
