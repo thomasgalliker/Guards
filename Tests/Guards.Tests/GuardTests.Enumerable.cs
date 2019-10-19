2fd8aa7d46941cfb69618b95bb0c9d91f6ec76a9
Thomas Galliker <thomas@bluewin.ch>
2019-10-19 17:21:49 +0200

    Add new test case

--- Tests/Guards.Tests/GuardTests.Enumerable.cs
+++ Tests/Guards.Tests/GuardTests.Enumerable.cs
@@ -10,33 +10,51 @@ namespace Guards.Tests
     public partial class GuardTests
     {
         [Fact]
-        public void ArgumentNotNullOrEmptyThrowsIfEnumerableIsEmpty()
+        public void ArgumentNotNullOrEmpty_DoesNotThrowIfEnumerableContainsItems()
         {
             // Arrange
-            string argumentName = "list";
-            var list = new List<string>();
+            var enumerable = new List<string> { "item1", "item2" };
+            string argumentName = nameof(enumerable);
 
             // Act
-            var ex = Assert.Throws<ArgumentException>(() => Guard.ArgumentNotNullOrEmpty(list, argumentName));
+            var enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Action action = () => Guard.ArgumentNotNullOrEmpty(enumerable, argumentName);
 
-            // Assert
-            ex.ParamName.Should().BeEquivalentTo(argumentName);
-            ex.Message.Should().NotBeNullOrEmpty();
+                // Assert
+                action.Should().NotThrow<ArgumentException>();
+            }
         }
 
         [Fact]
-        public void ArgumentNotNullOrEmptyThrowsIfEnumerableIsEmptyWithExpression()
+        public void ArgumentNotNullOrEmpty_ThrowsIfEnumerableIsEmpty()
         {
             // Arrange
-            string argumentName = "list";
-            var list = new List<string>();
+            var enumerable = new List<string>();
+            string argumentName = nameof(enumerable);
 
             // Act
-            var ex = Assert.Throws<ArgumentException>(() => Guard.ArgumentNotNullOrEmpty(() => list));
+            var ex = Assert.Throws<ArgumentException>(() => Guard.ArgumentNotNullOrEmpty(enumerable, argumentName));
 
             // Assert
             ex.ParamName.Should().BeEquivalentTo(argumentName);
-            ex.Message.Should().NotBeNullOrEmpty();
+            ex.Message.Should().Be("Argument must not be empty.\r\nParameter name: enumerable");
+        }
+
+        [Fact]
+        public void ArgumentNotNullOrEmpty_ThrowsIfEnumerableIsEmptyWithExpression()
+        {
+            // Arrange
+            var enumerable = new List<string>();
+            string argumentName = nameof(enumerable);
+
+            // Act
+            var ex = Assert.Throws<ArgumentException>(() => Guard.ArgumentNotNullOrEmpty(() => enumerable));
+
+            // Assert
+            ex.ParamName.Should().BeEquivalentTo(argumentName);
+            ex.Message.Should().Be("Argument must not be empty.\r\nParameter name: enumerable");
         }
     }
 }
