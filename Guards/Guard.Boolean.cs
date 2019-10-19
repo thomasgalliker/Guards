84293614ea0a71adaae8da1ed43acff9a6b2676f
Thomas Galliker <thomas@bluewin.ch>
2019-10-19 16:26:08 +0200

    Fix wrong exception message in ArgumentIsTrue and ArgumentIsFalse

--- Guards/Guard.Boolean.cs
+++ Guards/Guard.Boolean.cs
@@ -28,7 +28,7 @@ namespace Guards
         /// <exception cref="ArgumentException">The <paramref name="expression" /> parameter is false.</exception>
         public static void ArgumentIsTrue([ValidatedNotNull]Expression<Func<bool>> expression)
         {
-            ArgumentIsTrueOrFalse(expression, throwCondition: false, exceptionMessage: ExceptionMessages.ArgumentMustBeFalse);
+            ArgumentIsTrueOrFalse(expression, throwCondition: false, exceptionMessage: ExceptionMessages.ArgumentMustBeTrue);
         }
 
         /// <summary>
@@ -37,7 +37,7 @@ namespace Guards
         /// <exception cref="ArgumentException">The <paramref name="expression" /> parameter is true.</exception>
         public static void ArgumentIsFalse([ValidatedNotNull]Expression<Func<bool>> expression)
         {
-            ArgumentIsTrueOrFalse(expression, throwCondition: true, exceptionMessage: ExceptionMessages.ArgumentMustBeTrue);
+            ArgumentIsTrueOrFalse(expression, throwCondition: true, exceptionMessage: ExceptionMessages.ArgumentMustBeFalse);
         }
 
         private static void ArgumentIsTrueOrFalse(Expression<Func<bool>> expression, bool throwCondition, string exceptionMessage)
